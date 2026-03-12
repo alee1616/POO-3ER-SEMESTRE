9fdaeb3f3a45d1961d71574a0006a1c16cacca8c
alee1616 <maguillenc@uamv.edu.ni>
2026-03-11 21:34:17 -0600

    primer commit

---
+++ EJERCICIOS/src/ejercicios/Ejercicio1.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Nombres: Marian Alejandra");
+        System.out.println("Apellidos: Guillén Castillo");
+        System.out.println("Edad: 17 años");
+        System.out.println("Sexo: F");
+        System.out.println("Carrera: Ingeniería de Sistemas");
+    }
+
+}
+
