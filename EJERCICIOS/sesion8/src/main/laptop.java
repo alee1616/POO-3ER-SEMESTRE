146617e9bc50cc4dd7b214ef770015b6dd066c99
alee1616 <maguillenc@uamv.edu.ni>
2026-03-26 22:21:53 -0600

    Sesión 8

--- EJERCICIOS/sesion8/src/main/laptop.java
+++ EJERCICIOS/sesion8/src/main/laptop.java
@@ -3,10 +3,10 @@ import models.Laptop;
 public class laptop {
 
     public static void main(String[] args) {
-         Laptop aser  = new Laptop ();
-         aser.setMarca("ASER NITRO V 15");
-         aser.setPrecio(1600);
-         aser.setCantidad(1);
+        Laptop aser  = new Laptop ();
+        aser.setMarca("ASER NITRO V 15");
+        aser.setPrecio(1600);
+        aser.setCantidad(1);
 
         System.out.println(aser);
 
